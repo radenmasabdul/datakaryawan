b771cf9811dff0b56b59987d09c3b58f323bce6c
radenmasabdul <abdul.tamsis@gmail.com>
2022-12-20 22:46:47 +0700

    Inisialisasi Awal

---
+++ backend/routes/UserRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { getUsers, getUsersById, saveUser, updateUser, deleteUser } from "../controllers/UserControllers.js"; 
+
+const router = express.Router();
+
+router.get('/users', getUsers);
+router.get('/users/:id', getUsersById);
+router.post('/users', saveUser);
+router.patch('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+
+export default router;
